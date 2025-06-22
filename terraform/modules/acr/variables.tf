ef883dfea830d823f1b4881929e56cc8092ff616
devuser13 <devuser13@refabs365.com>
2025-06-22 06:21:09 +0000

    Deleted modules

--- terraform/modules/acr/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "rg_name" {
-  description = "The name of the resource group"
-  type        = string
-}
-
-variable "location" {
-  description = "The location for the resources"
-  type        = string
-}
